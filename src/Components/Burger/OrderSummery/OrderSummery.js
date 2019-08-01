0050ed71cf6209983c4487f15948e7b78e0e1494
Sneha Thadani <snehacopper@gmail.com>
2019-08-01 15:28:09 -0700

    Life CYcle Hooks Added

--- src/Components/Burger/OrderSummery/OrderSummery.js
+++ src/Components/Burger/OrderSummery/OrderSummery.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import Aux from '../../../HOC/Aux';
 import Button from '../../UI/Button/Button';
-const OrderSummery= (props)=> {
-    const ingredientSummery= Object.keys (props.ingredient)
+class OrderSummery extends React.Component  {
+    componentWillUpdate (){
+        console.log('[OrderSummery] will update');
+    }
+    render(){
+    const ingredientSummery= Object.keys (this.props.ingredient)
     .map(igKey=> {
         return (
         <li key={igKey}> 
-            <span style={{textTransform:'capitalize'}}>{igKey}</span>: {props.ingredient[igKey]}
+            <span style={{textTransform:'capitalize'}}>{igKey}</span>: {this.props.ingredient[igKey]}
              </li>);
-    })
+    });
     return(
     <Aux>
         <h3> Your Order</h3>
@@ -16,12 +20,13 @@ const OrderSummery= (props)=> {
         <ul>
             {ingredientSummery}
         </ul>
-        <p> <strong> Total Price:{props.price.toFixed(2)}</strong></p>
+        <p> <strong> Total Price:{this.props.price.toFixed(2)}</strong></p>
         <p> Continue to Check Out?</p>
-        <Button btnType="Danger"  clicked={props.purchaseCancelled}> CANCEL </Button>
-        <Button btnType="Success" clicked={props.purchaseContinued}> CONTINUE </Button>
+        <Button btnType="Danger"  clicked={this.props.purchaseCancelled}> CANCEL </Button>
+        <Button btnType="Success" clicked={this.props.purchaseContinued}> CONTINUE </Button>
     </Aux>
 );
     }
+}
 
 export default OrderSummery;
