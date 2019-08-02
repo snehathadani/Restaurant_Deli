86084b53a7daba4ddf03cec22657e2cbf8fcc644
Sneha Thadani <snehacopper@gmail.com>
2019-08-01 20:15:00 -0700

    lifecycle method

--- src/Components/UI/Modal/Modal.js
+++ src/Components/UI/Modal/Modal.js
@@ -2,17 +2,27 @@ import React from 'react';
 import classes from './Modal.module.css';
 import Aux from '../../../HOC/Aux';
 import BackDrop from '../BackDrop/BackDrop';
-const Modal= (props)=> (
+class Modal extends React.Component {
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextProps.show !== this.props.show;
+    }
+    componentWillUpdate(){
+        console.log('[Modal] will update');
+    }
+    render(){
+        return(
     <Aux>
-        <BackDrop show={props.show} clicked={props.modalClosed}/>
+        <BackDrop show={this.props.show} clicked={this.props.modalClosed}/>
     <div className={classes.Modal}
         style={{
-            transform:props.show ? 'translateY(0)': 'translateY(-100vh)',
-            opacity:props.show ? '1': '0'
+            transform:this.props.show ? 'translateY(0)': 'translateY(-100vh)',
+            opacity:this.props.show ? '1': '0'
         }}>
-        {props.children}
+        {this.props.children}
     </div>
     </Aux>
-)
+        )
+}
+}
 
 export default Modal;
